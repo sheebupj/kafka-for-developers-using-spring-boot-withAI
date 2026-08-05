aa2a4a21cd64357ee6ec66f0837effbac03aa68f
Sheebu P J <Sheebupj@gmail.com>
2026-08-05 17:08:37 +0530

    Refactor dependencies and improve Kafka logging levels in configuration and step one in implementation plan completed

--- lib-events-consumer-boot4/src/main/java/com/paremal/kafka/consumer/LibraryEventsConsumer.java
+++ lib-events-consumer-boot4/src/main/java/com/paremal/kafka/consumer/LibraryEventsConsumer.java
@@ -5,14 +5,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import com.paremal.kafka.service.LibraryEventService;
+import tools.jackson.core.JacksonException;
 
 @Component
 public class LibraryEventsConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(LibraryEventsConsumer.class);
+    private final LibraryEventService libraryEventService;
+
+    public LibraryEventsConsumer(LibraryEventService libraryEventService) {
+        this.libraryEventService = libraryEventService;
+    }
 
     @KafkaListener(topics = "library-events")
-    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) {
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) throws JacksonException {
         log.info(
                 "ConsumerRecord received. topic={}, partition={}, offset={}, key={}, value={}",
                 consumerRecord.topic(),
@@ -20,5 +27,6 @@ public class LibraryEventsConsumer {
                 consumerRecord.offset(),
                 consumerRecord.key(),
                 consumerRecord.value());
+        libraryEventService.processEvent(consumerRecord);
     }
 }
